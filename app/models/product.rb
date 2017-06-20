b7421150f0dc4d8d5f5ea867236ceeb5dfe16854
Nicolas <>
2017-06-20 22:02:19 +0200

    add category to products and category cards to index UI

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true
+  validates :tagline, presence: true
+  validates :category, inclusion: { in: %w(tech education design), message: "%{value} is not a valid category" }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,27 @@
 
 Product.destroy_all
 
-Product.create(name: "Skello", url: "https://www.skello.io", tagline: "Gérez vos plannings de façon simple et intelligente")
-Product.create(name: "Bonsai", url: "https://www.hellobonsai.com/", tagline: "Bulletproof contracts, e-signing, & invoices used by 40000+ top freelancers")
-Product.create(name: "Prisma", url: "https://prisma-ai.com", tagline: "AI that turns your photos into artwork in seconds")
-Product.create(name: "Email Hunter", url: "https://hunter.io", tagline: "Find all the email addresses related to a domain")
-Product.create(name: "Medium 2.0", url: "https://medium.com/", tagline: "A new look, app, and features to move thinking forward")
+Product.create(name: "Skello", url: "https://www.skello.io",
+tagline: "Gérez vos plannings de façon simple et intelligente",
+category: "tech")
+Product.create(name: "Bonsai", url: "https://www.hellobonsai.com/",
+tagline: "Bulletproof contracts, e-signing, & invoices used by 40000+ top freelancers",
+category: "tech")
+Product.create(name: "Prisma", url: "https://prisma-ai.com",
+tagline: "AI that turns your photos into artwork in seconds",
+category: "tech")
+Product.create(name: "Email Hunter", url: "https://hunter.io",
+tagline: "Find all the email addresses related to a domain",
+category: "tech")
+Product.create(name: "Medium 2.0", url: "https://medium.com/",
+tagline: "A new look, app, and features to move thinking forward",
+category: "tech")
+Product.create(name: "Le Wagon", url: "https://lewagon.com/",
+tagline: "Coding bootcamp for entrepreneurs and creative people",
+category: "education")
+Product.create(name: "Code4Startup", url: "https://code4startup.com/",
+tagline: "Learn how to code by cloning real life startups",
+category: "education")
+Product.create(name: "Coolors", url: "https://coolors.co",
+tagline: "Super fast color schemes generator for cool designers",
+category: "design")
