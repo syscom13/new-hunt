193b38d5f22803d1ae53d4a4c65d448f5ca2e05a
Nicolas <>
2017-06-20 02:04:15 +0200

    add UI for home and index pages + navbar + created seed and migration for tagline and url on products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+  def show
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
+
+  private
+  def product_params
+
+  end
+
+  def find_product
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+Product.create(name: "Skello", url: "https://www.skello.io", tagline: "Gérez vos plannings de façon simple et intelligente")
+Product.create(name: "Bonsai", url: "https://www.hellobonsai.com/", tagline: "Bulletproof contracts, e-signing, & invoices used by 40000+ top freelancers")
+Product.create(name: "Prisma", url: "https://prisma-ai.com", tagline: "AI that turns your photos into artwork in seconds")
+Product.create(name: "Email Hunter", url: "https://hunter.io", tagline: "Find all the email addresses related to a domain")
+Product.create(name: "Medium 2.0", url: "https://medium.com/", tagline: "A new look, app, and features to move thinking forward")
