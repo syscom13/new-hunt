acfe1081835b224c7ea8b0feb9cb0169c771be77
Nicolas <>
2017-06-21 13:46:03 +0200

    add user validation on product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
+  validates :user, presence: true
   validates :name, presence: true, uniqueness: true
   validates :url, presence: true, uniqueness: true
   validates :tagline, presence: true
